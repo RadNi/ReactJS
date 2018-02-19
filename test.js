4009be175c4fac765d51907c1cf04e75f83f809c
RadNi <amirhosseinkh@mail.ce.sharif.edu>
2018-02-19 12:34:58 +0330

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ let xml = new XMLHttpRequest()
 
 xml.open('GET', 'https://api.github.com', true)
 xml.setRequestHeader('Accept', 'application/vnd.github.v3+json')
-xml.setRequestHeader ("Authorization", "Basic " + Buffer.from("RadNi" + ":" + "Amirhossein11").toString('base64'));
+xml.setRequestHeader ("Authorization", "Basic " + Buffer.from("RadNi" + ":" + "my github password").toString('base64'));
 
 xml.send()
  xml.onreadystatechange = function() {
