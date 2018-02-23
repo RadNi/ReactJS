1c27f7830a72c86238308bf99be1f1e1018729eb
AMIRLILG\Amirali <gatmiri@ce.sharif.edu>
2018-02-23 16:32:41 +0330

    added power(x,n)

--- first.js
+++ first.js
@@ -2,5 +2,15 @@ function getSum(a,b) {
     return a + b;
 }
 
+function power(x,n) {
+    if (n==1){
+        return x;
+    }
+    else{
+        return x * power(x,n-1);
+    }
+
+}
 
 console.log(getSum(5,6));
+console.log(power(5,3));
